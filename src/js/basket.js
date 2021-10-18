2d949ceef1e68b8d24f2621f09f8a3a70c5ba2d8
azad <mohammadazad.bc~gmail.com>
2021-10-18 15:20:54 +0200

    add shop button

--- src/js/basket.js
+++ src/js/basket.js
@@ -1,5 +1,6 @@
 import { db_upgrade } from "./db";
-
+const shopBasket = document.getElementById('basket')
+const shopping = document.getElementById('shopping')
 
 export function Basket(){ 
 const charBasket = document.getElementById('char-basket')
@@ -57,18 +58,8 @@ for(let element of db_upgrade){
 
 }
 }
-
-
-
-/*
-        <div class="char-power">
-            <article class="article"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSs1Eht_eBrz2bhChpe4N-gDnIsiFQdbZLOx4C6gHiZ3gPR-y4h8MbzKn6ggPsFM7D_98&usqp=CAU" alt=""></article>
-            <article class="article"> <img src="https://thumbs.dreamstime.com/t/power-punch-hand-illustration-art-background-77718584.jpg" alt=""></article>
-            <article class="article"><img src="https://images.megapixl.com/4893/48936764.jpg" alt=""> </article>
-            <article class="article"><img src="https://s3-eu-west-1.amazonaws.com/cdn.webfactore.co.uk/sr_430113_largeish.jpg" alt=""></article>
-            <article class="article"><img src="https://m.media-amazon.com/images/I/81l-whkD3CL._AC_SL1500_.jpg" alt=""></article>
-            <article class="article"><img src="https://www.picclickimg.com/d/l400/pict/114828946564_/Cute-Lion-Cartoon-Face-Head-Circle-Gift-Idea.jpg" alt=""></article>
-            
-        </div>
-    </div>
-    */
+export function showBasket(){
+    shopping.addEventListener('click',()=>{
+        shopBasket.classList.toggle('show')
+    })
+}
